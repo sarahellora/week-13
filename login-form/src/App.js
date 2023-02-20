a2825892ace7e0bcf182d6e74dce163240b91c10
Sarahellora <sarahellora1996@gmail.com>
2023-02-20 13:48:49 -0700

    added login form

---
+++ login-form/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h3> Login Form</h3>
+     <div className="loginContainer">
+    UserName: <input></input>
+     Password: <input></input>
+      
+     </div>
+    </div>
+  );
+}
+
+export default App;
